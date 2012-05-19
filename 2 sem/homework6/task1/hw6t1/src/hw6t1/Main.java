64358c6e509acc72032d54f45ad123669d12326f
SabinaIdrisova <s.r.idrisova@gmail.com>
2012-05-20 00:43:24 +0400

    added calculator

---
+++ 2 sem/homework6/task1/hw6t1/src/hw6t1/Main.java
@@ -0,0 +1,12 @@
+package hw6t1;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.setVisible(true);
+    }
+}
